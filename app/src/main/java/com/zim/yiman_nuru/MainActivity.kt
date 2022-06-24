b1ef3abcc681f4d3e1ad2eb06ad9db80f33d8083
prince-tagaev <asaba@internet.ru>
2022-06-24 22:16:57 +0300

    created switches to night mode

--- app/src/main/java/com/zim/yiman_nuru/MainActivity.kt
+++ app/src/main/java/com/zim/yiman_nuru/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.zim.yiman_nuru
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.widget.SwitchCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.zim.yiman_nuru.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var switch: SwitchCompat
+
     lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
+    @SuppressLint("SwitchIntDef")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,14 +33,18 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawer)
         val navView: NavigationView = findViewById(R.id.navigation_view)
 
-        // Вверхные меню старт
+
+
+        // запуск ввернего меню
+
         val menu_btn: ImageView = findViewById(R.id.btn_menu)
         menu_btn.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
         }
-        // Вверхные меню енд
 
-        // боковое меню старт
+        // конец вверхнего меню
+
+        // запуск бокового меню
 
         navView.setNavigationItemSelectedListener {
             when (it.itemId){
@@ -45,13 +61,48 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        // боковое меню енд
+        // конец бокового меню
 
-        mainFragment()
-        buttons()
-        onClickHorizontalActivity()
+        mainFragment() // фрагмент главного экрана
+        onClickHorizontalActivity() // Горизонтальные кнопки (горизонтальное меню)
+
+//        Night mode switcher start
+
+        switch = findViewById(R.id.switch_btn_night_mode)
+//        when (AppCompatDelegate.getDefaultNightMode()){
+//            MODE_NIGHT_YES -> AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+//        }
+
+//        проверить ночной режим по умолчанию START
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO ||
+                AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM ||
+                AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY){
+            switch.isChecked = false // отключает кнопку Switch когда яркий режим телефона
+        }else if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_UNSPECIFIED ||
+            AppCompatDelegate.getDefaultNightMode() == MODE_NIGHT_YES
+        ){
+            switch.isChecked = true // включает кнопку Switch когда тёмный режим телефона
+        }
+
+        //        проверить ночной режим по умолчанию END
+
+
+//        переключить ночной режим START
+
+        switch.setOnCheckedChangeListener { compoundButton, b ->
+            if (b) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                onRestart()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                onRestart()
+            }
+        }
 
     }
+
+    //        переключить ночной режим END
+
 
     fun mainFragment(){
         val text_yiman_nuru: TextView = findViewById(R.id.text_yiman_nuru)
@@ -107,48 +158,5 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-
-    fun buttons() {
-
-//        val btnIslam_test: CardView = findViewById(R.id.btn_islam_test)
-//        val btnMasnunDubalar: CardView = findViewById(R.id.btn_masnun_dubalar)
-//        val btnTasbih: CardView = findViewById(R.id.btn_tasbih)
-//        val btn_alty_sypat: CardView = findViewById(R.id.btn_alty_sypat)
-//        val btn_zikir: CardView = findViewById(R.id.btn_zikir)
-//        val btn_allanyn_ysymdary: CardView = findViewById(R.id.btn_allanyn_ysymdary)
-//
-//        btn_allanyn_ysymdary.setOnClickListener {
-//            Toast.makeText(this, "Азыркы учурда бул баракча даяр эмес!", Toast.LENGTH_LONG).show()
-//        }
-//
-//        btn_zikir.setOnClickListener {
-//            val intent = Intent(this, Zikir::class.java)
-//            startActivity(intent)
-//        }
-////        val btn_namaz: CardView = findViewById(R.id.btn_namaz)
-//
-////        btn_namaz.setOnClickListener {
-////            Toast.makeText(this, "Азыркы учурда бул баракча түзүүчүлөр тараптан иштелип жатат кудай буюрса тиркеменин кийинки версияларында даяр болот.", Toast.LENGTH_LONG).show()
-////        }
-//
-//        btn_alty_sypat.setOnClickListener{
-//            val intent = Intent(this, AltySypat::class.java)
-//            startActivity(intent)
-//        }
-//        btnIslam_test.setOnClickListener{
-////            val intent = Intent(this, IslamTest::class.java)
-////            startActivity(intent)
-//            Toast.makeText(this, "Азыркы учурда бул баракча даяр эмес!", Toast.LENGTH_LONG).show()
-//        }
-//        btnMasnunDubalar.setOnClickListener{
-////            val intent = Intent(this, MasnunDubalar::class.java)
-////            startActivity(intent)
-//            Toast.makeText(this, "Азыркы учурда бул баракча даяр эмес!", Toast.LENGTH_LONG).show()
-//        }
-//        btnTasbih.setOnClickListener{
-//            val intent = Intent(this, Tasbih::class.java)
-//            startActivity(intent)
-//        }
-    }
 
 }
